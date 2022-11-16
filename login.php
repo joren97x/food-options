29f827b87ffb88625a67163dff5188a8b856e1da
Joren Sumagang <112835241+joren97x@users.noreply.github.com>
2022-11-16 16:55:18 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,12 +1,19 @@
 <?php
+require "conn.php";
     session_start();
+
+    if (isset($_SESSION['email'])) {
+
+        header("Location: index.php");
+
+    }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Food Options</title>
+    <title>Log In - Food Options</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.1/font/bootstrap-icons.css">
@@ -14,125 +21,54 @@
 <body>
 
         <div class="container">
-            <div class="row  mt-3 text-success">
-                <div class="col-1" >
-                <a href="login.php" class="text-success" style="text-decoration: none;">Log In</a>
+            <div class="row mt-3 text-success">
+                <div class="col-2 text-start" >
+                    <a href="create.php" class="text-success" style="text-decoration: none;"><?php if (!isset($_SESSION['email'])) { echo "<a href='login.php' class='text-success' style='text-decoration: none;'><i class='bi bi-box-arrow-in-right'></i> Log In</a>";} else {echo "<a href='myAccount.php' class='text-success' style='text-decoration: none;'><i class='bi bi-person-circle'></i> My Account</a>";}  ?></a>
                 </div>
-                <div class="col-2" >
-                    <a href="create.php" class="text-success" style="text-decoration: none;"> Create Account</a>
+                <div class="col-2 text-start" >
+                <?php if (!isset($_SESSION['email'])) { echo "<a href='create.php' class='text-success' style='text-decoration: none;'><i class='bi bi-person-add'></i> Create Account</a>";} else {echo "<a href='logOut.php' class='text-success' style='text-decoration: none;'> <i class='bi bi-box-arrow-right'></i> Log Out</a>";}  ?>
+
                 </div>
-                <div class="col-8 text-center">
+                <div class="col-7 text-center">
                     <i class="bi bi-search"></i>
                     Search for a food here
                 </div>
                 <div class="col-1">
                     <i class="bi bi-cart3"></i>
-                    Cart
+                    <a href='cart.php' class='text-success' style='text-decoration: none;'> Cart</a>
                 </div>
                 <hr class="mt-3">
             </div>
 
             <div class="row">
                 <div class="col display-4 text-center justify-content-center text-success">
-                    Food <i class="bi bi-egg"></i>ptions
+                    <a href="index.php" class="text-success " style="text-decoration: none;">Food <i class="bi bi-egg"></i>ptions</a>
                 </div>
             </div>
 
             <div class="row bg-success mt-3 text-center p-2">
-                <div class="col">
-                    <div class="dropdown">
-                        <button class=" btn btn-success dropdown-toggle"  data-bs-toggle="dropdown" >Eggs</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">Small Eggs</a> 
-                            <a class="dropdown-item" href="">Medium Eggs</a>
-                            <a class="dropdown-item" href="">Big Eggs</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="dropdown">
-                        <button class=" btn btn-success dropdown-toggle"  data-bs-toggle="dropdown" >Fruits</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">Apple</a>
-                            <a class="dropdown-item" href="">Avocado</a>
-                            <a class="dropdown-item" href="">Coconut</a>
-                            <a class="dropdown-item" href="">Grapes</a>
-                            <a class="dropdown-item" href="">Pineapple</a>
-                            <a class="dropdown-item" href="">Papaya</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="dropdown">
-                        <button class=" btn btn-success dropdown-toggle"  data-bs-toggle="dropdown" >Vegetables</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">Small Eggs</a>
-                            <a class="dropdown-item" href="">Medium Eggs</a>
-                            <a class="dropdown-item" href="">Big Eggs</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="dropdown">
-                        <button class=" btn btn-success dropdown-toggle"  data-bs-toggle="dropdown" >Meat</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">Small Eggs</a>
-                            <a class="dropdown-item" href="">Medium Eggs</a>
-                            <a class="dropdown-item" href="">Big Eggs</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="dropdown">
-                        <button class=" btn btn-success dropdown-toggle"  data-bs-toggle="dropdown" >Seafood</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">Small Eggs</a>
-                            <a class="dropdown-item" href="">Medium Eggs</a>
-                            <a class="dropdown-item" href="">Big Eggs</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="dropdown">
-                        <button class=" btn btn-success dropdown-toggle"  data-bs-toggle="dropdown" >Fast Food</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">Small Eggs</a>
-                            <a class="dropdown-item" href="">Medium Eggs</a>
-                            <a class="dropdown-item" href="">Big Eggs</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="dropdown">
-                        <button class=" btn btn-success dropdown-toggle"  data-bs-toggle="dropdown" >Bread</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">Small Eggs</a>
-                            <a class="dropdown-item" href="">Medium Eggs</a>
-                            <a class="dropdown-item" href="">Big Eggs</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="dropdown">
-                        <button class=" btn btn-success dropdown-toggle"  data-bs-toggle="dropdown" >Herbs & Spices</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">Small Eggs</a>
-                            <a class="dropdown-item" href="">Medium Eggs</a>
-                            <a class="dropdown-item" href="">Big Eggs</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="dropdown">
-                        <button class=" btn btn-success dropdown-toggle"  data-bs-toggle="dropdown" >Drinks</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="">Small Eggs</a>
-                            <a class="dropdown-item" href="">Medium Eggs</a>
-                            <a class="dropdown-item" href="">Big Eggs</a>
-                        </div>
-                    </div>
-                </div>
 
+            <?php
+
+                $query = "SELECT * FROM tblCategory";
+                $result = mysqli_query($conn, $query);
+
+                if(mysqli_num_rows($result) > 0) {
+
+                    foreach($result as $category) {
+
+            ?>
+                <div class="col">
+                        <form action="" method="GET">
+                            <a href="functions.php"><button class=" btn btn-success " name="btnCategory" id="btnCategory" value="<?php echo $category['id']; ?>" ><?php  echo $category['categoryName']; ?></button></a>
+                        </form>
+                </div>
+            <?php
+
+                    }
+                }
+
+            ?>
             </div>
 
             <hr>
@@ -162,17 +98,17 @@
                     <div class="col">Follow Us</div>
                 </div>
                 <div class="row">
-                    <div class="col">Create Account</div>
-                    <div class="col">About Us</div>
+                    <div class="col"><a href='create.php' class='text-success' style='text-decoration: none;'>Create Account</a></div>
+                    <div class="col"><a href='aboutUs.php' class='text-success' style='text-decoration: none;'>About Us</a></div>
                     <div class="col"><i class="bi bi-instagram"></i> Instagram</div>
                 </div>
                 <div class="row">
-                    <div class="col">My Account</div>
+                    <div class="col"><a href='myAccount.php' class='text-success' style='text-decoration: none;'>My Account</a></div>
                     <div class="col">Privacy Policy</div>
                     <div class="col"><i class="bi bi-facebook"></i> Facebook</div>
                 </div>
                 <div class="row">
-                    <div class="col">Log In</div>
+                    <div class="col"><a href='logIn.php' class='text-success' style='text-decoration: none;'>Log In</a></div>
                     <div class="col">Hello</div>
                     <div class="col"><i class="bi bi-youtube"></i> Youtube</div>
                 </div>
